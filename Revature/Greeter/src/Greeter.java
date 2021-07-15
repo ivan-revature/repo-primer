b0334db2cd04200adee783b2a60173192dee02df
ivan-revature <ivan.fletes@revature.net>
2021-07-14 17:22:32 -0700

    submit HelloWorld and NameConsole labs

---
+++ Revature/Greeter/src/Greeter.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ivan_
+ *
+ */
+public class Greeter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+	}
+
+}
---
+++ Revature/NameConsole/src/Greeter.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ivan_
+ *
+ */
+public class Greeter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Ivan Fletes! :-)");
+	}
+
+}
